484a187a54173bb0801a41450f8fbd61aeafd377
d-burgess <43961478+d-burgess@users.noreply.github.com>
2020-08-08 11:34:01 +0100

    Initial commit

---
+++ midi-program-changer.h
@@ -0,0 +1,18 @@
+#ifndef __MidiProgramChanger__
+#define __MidiProgramChanger__
+
+// Set serial port
+#if defined( TEENSYDUINO ) 
+  #if defined( __MK20DX256__ )       
+    #define BOARD "Teensy 3.2"
+    #define HWSERIAL Serial1
+  #endif
+#elif defined( ARDUINO_AVR_UNO )       
+  #define BOARD "Arduino Uno"
+  #define HWSERIAL Serial
+#else
+  #define BOARD "Unknown"
+  #define HWSERIAL Serial
+#endif
+
+#endif
